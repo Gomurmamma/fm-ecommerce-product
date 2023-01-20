3d0b7e959d505c46633db1c951060216ab154bfe
Gomurmamma <dnaldyoung@gmail.com>
2023-01-20 13:21:41 -0500

    playwright e2e testing set up

---
+++ e2e/index.spec.ts
@@ -0,0 +1,9 @@
+// tests/index.spec.ts
+import test from "./next-fixture";
+import { expect } from "@playwright/test";
+
+test("page text", async ({ page, port }) => {
+  await page.goto(`http://localhost:${port}/`);
+  const text = await page.innerText("h1");
+  expect(text).toBe("Hello world");
+});
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function HomePage() {
-  return <div>Hello world</div>;
+  return <h1>Hello world</h1>;
 }
 
 export default HomePage;
