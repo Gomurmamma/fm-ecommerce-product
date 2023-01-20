d487e7651718d6aec0cf0ceee2f93b459aba6f72
Gomurmamma <dnaldyoung@gmail.com>
2023-01-20 13:51:08 -0500

    defined Document file

---
+++ pages/_document.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+  render() {
+    //const setInitialTheme = `
+    //function getUserPreference() {
+    //  if(window.localStorage.getItem('theme')) {
+    //    return window.localStorage.getItem('theme')
+    //  }
+    //  return window.matchedMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+    //}
+    //document.body.dataset.theme = getUserPreference();
+    //`;
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#da532c"></meta>
+          <meta name="theme-color" content="#ffffff"></meta>
+        </Head>
+        <body>
+          {/*<script dangerouslySetInnerHTML={{ __html: setInitialTheme }} /> */}
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
