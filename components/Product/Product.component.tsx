a0f74bd4b1fb62b94d2446c28baae94cc1a99a1c
Gomurmamma <dnaldyoung@gmail.com>
2023-01-21 19:58:12 -0500

    Carousel state, updateIndex(), WIP

--- components/Product/Product.component.tsx
+++ components/Product/Product.component.tsx
@@ -1,19 +1,47 @@
+import React, { useState } from "react";
 import style from "./Product.module.scss";
 import Image from "next/image";
 
 type Props = {
   imageData: { filePath: string; altText: string }[];
+  children?: React.ReactNode;
 };
 
-function Product({ imageData }: Props): JSX.Element {
+function Product({ imageData, children }: Props): JSX.Element {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const updateIndex = (newIndex: any): void => {
+    console.log(newIndex);
+
+    if (newIndex < 0) {
+      newIndex = 0;
+    } else if (newIndex >= React.Children.count(children)) {
+      newIndex = React.Children.count(children) - 1;
+    }
+
+    setActiveIndex(newIndex);
+  };
+
   return (
     <figure className={style.Product}>
-      <button> {`>`} </button>
-      <button> {`<`} </button>
+      <button
+        onClick={() => {
+          updateIndex(activeIndex - 1);
+        }}
+      >
+        next
+      </button>
+      <button
+        onClick={() => {
+          updateIndex(activeIndex + 1);
+        }}
+      >
+        prev
+      </button>
       <div className={style.Carousel}>
         <ul
           className={style.Carousel__inner}
-          style={{ transform: "translate(-0%)" }}
+          style={{ transform: `translateX(-${activeIndex * 100}%)` }}
         >
           {imageData ? (
             imageData.map((image, index) => {
