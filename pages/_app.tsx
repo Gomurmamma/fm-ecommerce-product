faeb3ca20800f70a38f1fd43d21fc78b78bcddcc
Gomurmamma <dnaldyoung@gmail.com>
2023-01-26 16:01:29 -0500

    fixed import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { store } from "../redux/store";
 import Head from "next/head";
 import Navbar from "../components/Navbar/Navbar.component";
 import menuLinksData from "../components/Navbar/MenuLinksData";
@@ -14,14 +16,16 @@ const App = ({ Component, pageProps }: AppProps) => {
         <meta name="viewport" content="width=device-width" initial-scale="1" />
         <meta httpEquiv="X-UA-Compatible" content="IE-Edge" />
       </Head>
-      <Navbar
-        menuLinksData={menuLinksData}
-        userProfile={{
-          filePath: "/image-avatar.png",
-          altText: "Profile Image of User",
-        }}
-      ></Navbar>
-      <Component {...pageProps} />
+      <Provider store={store}>
+        <Navbar
+          menuLinksData={menuLinksData}
+          userProfile={{
+            filePath: "/image-avatar.png",
+            altText: "Profile Image of User",
+          }}
+        ></Navbar>
+        <Component {...pageProps} />
+      </Provider>
     </>
   );
 };
