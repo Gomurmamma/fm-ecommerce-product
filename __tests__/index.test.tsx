36c47fc4537f4625f0db1dca9387a1048f7338fe
Gomurmamma <dnaldyoung@gmail.com>
2023-02-01 16:33:37 -0500

    Updated Homepage unit test

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import "@testing-library/jest-dom";
+import App from "../pages/_app";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
 import { render, screen } from "@testing-library/react";
 import HomePage from "../pages/index";
 
-// describe("HomePage", () => {
-//   it("renders without crashing", () => {
-//     expect(() => render(<HomePage />)).not.toThrow();
-//   });
-// });
+describe("HomePage", () => {
+  const initialState = { cartmodal: false };
+  const mockStore = configureStore();
+  let store;
+
+  it("renders without crashing", () => {
+    store = mockStore(initialState);
+    expect(() =>
+      render(
+        <Provider store={store}>
+          <HomePage />
+        </Provider>
+      )
+    ).not.toThrow();
+  });
+});
